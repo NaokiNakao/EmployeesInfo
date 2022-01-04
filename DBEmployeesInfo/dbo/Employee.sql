5fb9e739501d9a7cec96a54b82d21393af9fffd7
Naoki Nakao <naokinakao.nn@gmail.com>
2022-01-03 19:22:26 -0800

    Add project database

---
+++ DBEmployeesInfo/dbo/Employee.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[Employee]
+(
+	[Id] INT NOT NULL PRIMARY KEY IDENTITY, 
+    [EmployeeId] INT NOT NULL, 
+    [FirstName] VARCHAR(50) NOT NULL, 
+    [LastName] VARCHAR(50) NOT NULL, 
+    [EmailAddress] VARCHAR(100) NOT NULL, 
+    [Location] VARCHAR(50) NOT NULL
+)
